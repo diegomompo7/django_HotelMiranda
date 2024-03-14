825a828288510da51ee86239bc47320476a69f0b
diegomompo7 <diego.mompo.redoli@gmail.com>
2024-03-14 13:19:33 +0100

    fix static

--- hotelMiranda/settings.py
+++ hotelMiranda/settings.py
@@ -131,11 +131,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/website/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    "/var/www/static/",
+    "/var/www/miranda-static/static/",
 ]
 STATIC_ROOT = '/var/www/miranda-static/static/'
 
