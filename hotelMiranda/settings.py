63925f8d6ea4ce5d2f4a1a4785521dd00daf7603
diegomompo7 <diego.mompo.redoli@gmail.com>
2024-04-09 13:43:38 +0200

    fix yml

--- hotelMiranda/settings.py
+++ hotelMiranda/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware",
+    'website.custom_middleware.CustomMiddleware'
 ]
 
 ROOT_URLCONF = 'website.urls'
